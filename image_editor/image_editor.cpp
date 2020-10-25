ed7dc782befaa13b3dc772581c54fea2ad78fd5a
Sergey Pavlov <psvvsp@inbox.ru>
2020-10-25 16:22:12 +0300

    Hello CMake!

---
+++ image_editor/image_editor.cpp
@@ -0,0 +1,12 @@
+﻿// image_editor.cpp: определяет точку входа для приложения.
+//
+
+#include "image_editor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
